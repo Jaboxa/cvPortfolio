e7eb8b331ac75dd336f273af81943ccd11aaf8ba
Jessika Tibblin <jessikatibblin@hotmail.com>
2018-11-26 09:37:19 +0100

    first commit

---
+++ cv.js
@@ -0,0 +1,278 @@
+$( document ).ready(function() {
+
+	$("#stickFigure").click(function(){
+	 	$("#stickFigure").css("display", "none");
+		$("#selfiePicture").css("display", "block");
+	});
+
+	$("#selfiePicture").click(function(){
+	 	$("#stickFigure").css("display", "none");
+		$("#selfiePicture2").css("display", "block");
+		$("body").css("background", "url(images/bg_winter_lake.JPG)no-repeat center fixed");
+		$("body").css("background-size", "cover");
+		/*open textbox, and the next click is going to be on that*/
+		$("#startBtn").css("display", "inline-block");
+
+	});
+
+	$("#startBtn").click(function(){
+	 	$("#stickFigure").css("display", "none");
+		$("#selfiePicture").css("display", "block");
+		$("body").css("background", "url(images/berriesLeaves.jpeg)no-repeat center fixed");
+		$("body").css("background-size", "cover");
+		/*open textbox, and the next click is going to be on that*/
+		$("#startBtn").css("display", "none");
+		$(".popUp").css("display", "block");
+		$("#textPopUp1").css("display", "block");
+		$("#textPopUp2").css("display", "none");
+		$("#textPopUp3").css("display", "none");
+		$("#textPopUp4").css("display", "none");
+		$("#textPopUp5").css("display", "none");
+		$("#textPopUp6").css("display", "none");
+		$("#textPopUp7").css("display", "none");
+		$("#textPopUp8").css("display", "none");
+		$("#textPopUp9").css("display", "none");
+		$("#textPopUp10").css("display", "none");
+	});
+
+	$(".tp1").click(function(){
+	 	$("#stickFigure").css("display", "none");
+		$("#selfiePicture").css("display", "block");
+		$("body").css("background", "url(images/leaves.png)no-repeat top fixed");
+		$("body").css("background-size", "cover");
+		/*open textbox, and the next click is going to be on that*/
+		/*crop image and chose top instead*/
+		$("#startBtn").css("display", "none");
+		$(".popUp").css("display", "block");
+		$("#textPopUp1").css("display", "none");
+		$("#textPopUp2").css("display", "block");
+		$("#textPopUp3").css("display", "none");
+		$("#textPopUp4").css("display", "none");
+		$("#textPopUp5").css("display", "none");
+		$("#textPopUp6").css("display", "none");
+		$("#textPopUp7").css("display", "none");
+		$("#textPopUp8").css("display", "none");
+		$("#textPopUp9").css("display", "none");
+		$("#textPopUp10").css("display", "none");
+	});
+
+	$(".tp2").click(function(){
+	 	$("#stickFigure").css("display", "none");
+		$("#selfiePicture").css("display", "block");
+		$("body").css("background", "url(images/wTree.jpeg)no-repeat bottom fixed");
+		/*open textbox, and the next click is going to be on that*/
+		$("#startBtn").css("display", "none");
+		$(".popUp").css("display", "block");
+		$("#textPopUp1").css("display", "none");
+		$("#textPopUp2").css("display", "none");
+		$("#textPopUp3").css("display", "block");
+		$("#textPopUp4").css("display", "none");
+		$("#textPopUp5").css("display", "none");
+		$("#textPopUp6").css("display", "none");
+		$("#textPopUp7").css("display", "none");
+		$("#textPopUp8").css("display", "none");
+		$("#textPopUp9").css("display", "none");
+		$("#textPopUp10").css("display", "none");
+	});
+
+	$(".tp3").click(function(){
+	 	$("#stickFigure").css("display", "none");
+		$("#selfiePicture").css("display", "block");
+		$("body").css("background", "url(images/train.jpg)no-repeat center fixed");
+		$("body").css("background-size", "cover");
+		/*open textbox, and the next click is going to be on that*/
+		$("#startBtn").css("display", "none");
+		$(".popUp").css("display", "block");
+		$("#textPopUp1").css("display", "none");
+		$("#textPopUp2").css("display", "none");
+		$("#textPopUp3").css("display", "none");
+		$("#textPopUp4").css("display", "block");
+		$("#textPopUp5").css("display", "none");
+		$("#textPopUp6").css("display", "none");
+		$("#textPopUp7").css("display", "none");
+		$("#textPopUp8").css("display", "none");
+		$("#textPopUp9").css("display", "none");
+		$("#textPopUp10").css("display", "none");
+	});
+	
+	$(".tp4").click(function(){
+	 	$("#stickFigure").css("display", "none");
+		$("#selfiePicture").css("display", "block");
+		$("body").css("background", "url(images/cups4.png)no-repeat center fixed");
+		$("body").css("background-size", "cover");
+		/*open textbox, and the next click is going to be on that*/
+		$("#startBtn").css("display", "none");
+		$(".popUp").css("display", "block");
+		$("#textPopUp1").css("display", "none");
+		$("#textPopUp2").css("display", "none");
+		$("#textPopUp3").css("display", "none");
+		$("#textPopUp4").css("display", "none");
+		$("#textPopUp5").css("display", "block");
+		$("#textPopUp6").css("display", "none");
+		$("#textPopUp7").css("display", "none");
+		$("#textPopUp8").css("display", "none");
+		$("#textPopUp9").css("display", "none");
+		$("#textPopUp10").css("display", "none");
+	});
+	
+	$(".tp5").click(function(){
+	 	$("#stickFigure").css("display", "none");
+		$("#selfiePicture").css("display", "block");
+		$("body").css("background", "url(images/grizzly.jpg)no-repeat center fixed");
+		$("body").css("background-size", "cover");
+		/*open textbox, and the next click is going to be on that*/
+		$("#startBtn").css("display", "none");
+		$(".popUp").css("display", "block");
+		$("#textPopUp1").css("display", "none");
+		$("#textPopUp2").css("display", "none");
+		$("#textPopUp3").css("display", "none");
+		$("#textPopUp4").css("display", "none");
+		$("#textPopUp5").css("display", "none");
+		$("#textPopUp6").css("display", "block");
+		$("#textPopUp7").css("display", "none");
+		$("#textPopUp8").css("display", "none");
+		$("#textPopUp9").css("display", "none");
+		$("#textPopUp10").css("display", "none");
+	});
+	
+	$(".tp6").click(function(){
+	 	$("#stickFigure").css("display", "none");
+		$("#selfiePicture").css("display", "block");
+		$("body").css("background", "url(images/mask1.png)no-repeat center fixed");
+		$("body").css("background-size", "cover");
+		/*open textbox, and the next click is going to be on that*/
+		$("#startBtn").css("display", "none");
+		$(".popUp").css("display", "block");
+		$("#textPopUp1").css("display", "none");
+		$("#textPopUp2").css("display", "none");
+		$("#textPopUp3").css("display", "none");
+		$("#textPopUp4").css("display", "none");
+		$("#textPopUp5").css("display", "none");
+		$("#textPopUp6").css("display", "none");
+		$("#textPopUp7").css("display", "block");
+		$("#textPopUp8").css("display", "none");
+		$("#textPopUp9").css("display", "none");
+		$("#textPopUp10").css("display", "none");
+	});
+	
+	$(".tp7").click(function(){
+	 	$("#stickFigure").css("display", "none");
+		$("#selfiePicture").css("display", "block");
+		$("body").css("background", "url(images/grey.jpg)no-repeat center fixed");
+		$("body").css("background-size", "cover");
+		/*open textbox, and the next click is going to be on that*/
+		$("#startBtn").css("display", "none");
+		$(".popUp").css("display", "block");
+		$("#textPopUp1").css("display", "none");
+		$("#textPopUp2").css("display", "none");
+		$("#textPopUp3").css("display", "none");
+		$("#textPopUp4").css("display", "none");
+		$("#textPopUp5").css("display", "none");
+		$("#textPopUp6").css("display", "none");
+		$("#textPopUp7").css("display", "none");
+		$("#textPopUp8").css("display", "block");
+		$("#textPopUp9").css("display", "none");
+		$("#textPopUp10").css("display", "none");
+	});
+
+	$(".tp8").click(function(){
+	 	$("#stickFigure").css("display", "none");
+		$("#selfiePicture").css("display", "block");
+		$("body").css("background", "url(images/bg_waterlogged_land.JPG)no-repeat center fixed");
+		$("body").css("background-size", "cover");
+		/*open textbox, and the next click is going to be on that*/
+		$("#startBtn").css("display", "none");
+		$(".popUp").css("display", "block");
+		$("#textPopUp1").css("display", "none");
+		$("#textPopUp2").css("display", "none");
+		$("#textPopUp3").css("display", "none");
+		$("#textPopUp4").css("display", "none");
+		$("#textPopUp5").css("display", "none");
+		$("#textPopUp6").css("display", "none");
+		$("#textPopUp7").css("display", "none");
+		$("#textPopUp8").css("display", "none");
+		$("#textPopUp9").css("display", "block");
+		$("#textPopUp10").css("display", "none");
+	});
+
+	$(".tp9").click(function(){
+	 	$("#stickFigure").css("display", "none");
+		$("#selfiePicture").css("display", "block");
+		$("body").css("background", "url(images/skyBerry.jpg)no-repeat center fixed");
+		$("body").css("background-size", "cover");
+		/*open textbox, and the next click is going to be on that*/
+		$("#startBtn").css("display", "none");
+		$(".popUp").css("display", "block");
+		$("#textPopUp1").css("display", "none");
+		$("#textPopUp2").css("display", "none");
+		$("#textPopUp3").css("display", "none");
+		$("#textPopUp4").css("display", "none");
+		$("#textPopUp5").css("display", "none");
+		$("#textPopUp6").css("display", "none");
+		$("#textPopUp7").css("display", "none");
+		$("#textPopUp8").css("display", "none");
+		$("#textPopUp9").css("display", "none");
+		$("#textPopUp10").css("display", "block");
+
+	});	 	
+
+	$(".tp10").click(function(){
+	 	$("#stickFigure").css("display", "block");
+		$("#selfiePicture").css("display", "none");
+		$("body").css("background", "url(images/beach.JPG)no-repeat bottom fixed");
+		$("body").css("background-size", "cover");
+		/*open textbox, and the next click is going to be on that*/
+		$("#startBtn").css("display", "none");
+		$(".popUp").css("display", "none");
+		$("#textPopUp1").css("display", "none");
+		$("#textPopUp2").css("display", "none");
+		$("#textPopUp3").css("display", "none");
+		$("#textPopUp4").css("display", "none");
+		$("#textPopUp5").css("display", "none");
+		$("#textPopUp6").css("display", "none");
+		$("#textPopUp7").css("display", "none");
+		$("#textPopUp8").css("display", "none");
+		$("#textPopUp9").css("display", "none");
+		$("#textPopUp10").css("display", "none");
+	});
+
+
+	$("button#close").click(function(){
+		$(".test").toggle();
+	  	$(".simpleSelfie").css("margin", "auto");
+	  	
+	});
+
+	$("button#close2").click(function(){
+	 	$("#facts").css("display","none");
+	  	
+	});
+
+	$("#contact").click(function(){
+		$(".test").toggle();
+	  	$(".simpleSelfie").css("margin", "auto");
+	  	$("#contactText").css("display","inline-block");
+	  	$("#presText").css("display","none");
+		$("#educationText").css("display","none");
+		$("#workText").css("display","none");
+	});
+ 
+
+ 	$(".forPrint").click(function(){
+ 		$("#facts").css("display","inline-block");
+ 		$("#facts").css("height", "100%");
+
+	});	
+
+ 	$('#printButton').on('click', function() {  
+ 		$("#facts").css("height", "95%");
+  		window.print();  
+  		return false; 
+	});
+
+});
+
+
+
+		
+ 		
